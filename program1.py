80483e01a5b82d791035775a4c5d32b908084850
vsrija <130125584+srijavurimella@users.noreply.github.com>
2024-11-11 15:14:07 +0530

    Update program1.py

--- program1.py
+++ program1.py
@@ -1,10 +1,23 @@
-class Solution(object):
+class Solution:
     def isValid(self, s):
         """
         :type s: str
         :rtype: bool
         """
-        pass
+        bracket_map = {')': '(', '}': '{', ']': '['}
+        stack = []
+        
+        for char in s:
+            if char in bracket_map:
+                top_element = stack.pop() if stack else '#'
+                if bracket_map[char] != top_element:
+                    return False
+            else:
+                stack.append(char)
+        
+        return not stack
+
+    
 
 
 
